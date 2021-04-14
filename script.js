9092d90f098fed4946204af0849f7fc1a452662a
joaopmo <joao.oliveira@dcc.ufmg.br>
2021-04-13 22:15:54 -0300

    basic operations

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
 function Viewport() {
-    let vh = window.innerHeight * 0.01;
-    let vw = window.innerWidth * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-
-    window.addEventListener('resize', () => {
+    let setViewport = function () {
         let vh = window.innerHeight * 0.01;
         let vw = window.innerWidth * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
         document.documentElement.style.setProperty('--vw', `${vw}px`);
-    });
+    };
+
+    setViewport();
+    window.addEventListener('resize', setViewport);
 }
 
 function FontSize() {
-    let form = Array.from(document.getElementById('buttons'));
-    let viewport = Math.min((window.innerHeight * 0.01), (window.innerWidth * 0.01));
+    let setFontSize = function () {
+        let form = Array.from(document.getElementById('buttons'));
+        let viewport = Math.min((window.innerHeight * 0.01), (window.innerWidth * 0.01));
 
-    form.forEach(function (item) {
-        let normal = 0;
-        switch (item.defaultValue.length) {
-            case 1:
-                normal = 1;
-                break;
-            case 2:
-                normal = 1.3;
-                break;
-            case 3:
-                normal = 1.6;
-            break;
-        }
-
-        item.style.fontSize = `${(9 * viewport) / normal}px`;
-    });
-
-    window.addEventListener('resize', () => {
-        viewport = Math.min((window.innerHeight * 0.01), (window.innerWidth * 0.01));
         form.forEach(function (item) {
             let normal = 0;
             switch (item.defaultValue.length) {
@@ -46,119 +26,172 @@ function FontSize() {
                     break;
                 case 3:
                     normal = 1.6;
-                    break;
             }
             item.style.fontSize = `${(9 * viewport) / normal}px`;
         });
-    });
+    };
+
+    setFontSize();
+    window.addEventListener('resize', setFontSize);
+}
+
+function InputEventStyle(event, target) {
+    switch (event.type) {
+        case 'pointerover':
+            if (event.pointerType === 'mouse')
+                target.style.boxShadow = `1px 1px 3px 2px var(--${target.className}), 
+                                      inset 0 0 60px 10px var(--${target.className}-alpha)`;
+            break;
+        case 'pointerout':
+            target.style.boxShadow = `1px 1px 3px 2px var(--${target.className})`;
+            target.style.removeProperty('transform');
+            target.style.textShadow = `1px 1px var(--${target.className})`;
+            break;
+        case 'pointerdown':
+            target.style.boxShadow = `0 0 3px 2px var(--${target.className}), 
+                                  inset 0 0 60px 10px var(--${target.className}-alpha)`;
+            target.style.transform = 'translate(1px, 3px)';
+            target.style.textShadow = `0 0 3px var(--${target.className})`;
+            break;
+        case 'pointerup':
+            if (event.pointerType === 'mouse')
+                target.style.boxShadow = `1px 1px 3px 2px var(--${target.className}), 
+                                  inset 0 0 60px 10px var(--${target.className}-alpha)`;
+            target.style.removeProperty('transform');
+            target.style.textShadow = `1px 1px var(--${target.className})`;
+            break;
+    }
 }
 
 function ButtonsHover() {
     let form = document.getElementById('buttons');
 
     form.addEventListener('pointerover', function (e) {
-        let x = e.clientX;
-        let y = e.clientY;
-        let target = document.elementFromPoint(x, y);
-
+        let target = document.elementFromPoint(e.clientX, e.clientY);
         if ((target !== form) && (e.isPrimary)) {
-            if (e.pointerType === 'mouse') {
-                target.style.boxShadow = `1px 1px 3px 2px var(--${target.className}), 
-                                  inset 0 0 60px 10px var(--${target.className}-alpha)`;
-            }
-
-            target.addEventListener('pointerout', function () {
-                this.style.boxShadow = `1px 1px 3px 2px var(--${target.className})`;
-                this.style.removeProperty('transform');
-                this.style.textShadow = `1px 1px var(--${this.className})`;
+            InputEventStyle(e, target);
+            target.addEventListener('pointerout', function (event) {
+                InputEventStyle(event, this);
             });
         }
     });
 
     form.addEventListener('pointerdown', function (e) {
-        let x = e.clientX;
-        let y = e.clientY;
-        let target = document.elementFromPoint(x, y);
-
+        let target = document.elementFromPoint(e.clientX, e.clientY);
         if ((target !== form) && (e.isPrimary)) {
-
-            target.style.boxShadow = `0 0 3px 2px var(--${target.className}), 
-                                  inset 0 0 60px 10px var(--${target.className}-alpha)`;
-            target.style.transform = 'translate(1px, 3px)';
-            target.style.textShadow = `0 0 3px var(--${target.className})`;
-
-            target.addEventListener('pointerup', function () {
-                if (e.pointerType === 'mouse') {
-                    this.style.boxShadow = `1px 1px 3px 2px var(--${target.className}), 
-                                  inset 0 0 60px 10px var(--${target.className}-alpha)`;
-                }
-                this.style.removeProperty('transform');
-                this.style.textShadow = `1px 1px var(--${this.className})`;
+            InputEventStyle(e, target);
+            target.addEventListener('pointerup', function (event) {
+                InputEventStyle(event, this)
             });
+
+            Operate(target);
+            DisplayUpdate();
         }
     });
 }
 
 let operation = {
-    first: null,
-    op: null,
-    second: null,
-    current: null,
+    first: '',
+    op: '',
+    second: '',
+    current: 'first',
+    result: '',
     calc() {
-      if (this.current === 3) {
-          switch (this.op) {
-              case 0:
-                  return add(this.first, this.second);
-              case 1:
-                  return multiply(this.first, this.second);
-              case 2:
-                  return div(this.first, this.second);
-              case  3:
-                  return mod(this.first, this.second);
-              case 4:
-                  return power(this.first, this.second);
-              default:
-                  this.first = null;
-                  this.op = null;
-                  this.second = null;
-                  this.current = null;
-          }
-      }
+        switch (this.op) {
+            case '+':
+                return +this.first + +this.second;
+            case '-':
+                return +this.first - +this.second;
+            case '×':
+                return +this.first * +this.second;
+            case '÷':
+                return +this.first / +this.second;
+            case '%':
+                return +this.first % +this.second;
+            case '^':
+                return (+this.first) ** +this.second;
+        }
     },
 };
 
-function add(a, b) {
-    return a + b;
-}
 
-function multiply(a, b) {
-    return a * b;
-}
+function Operate(key) {
+    if (key.className === 'number' || (key.id === 'subtract-key' && operation[operation.current].length === 0)) {
+        if (operation.result !== '') {
+            operation.first = '';
+            operation.op = '';
+            operation.second = '';
+            operation.current = 'first';
+            operation.result = '';
+        }
 
-function div(a, b) {
-    return a / b;
-}
+        if (key.defaultValue === '.' && !operation[operation.current].includes('.')) {
+            if (operation[operation.current].length >= 2) {
+                operation[operation.current] += key.defaultValue;
+            } else if (operation[operation.current].length !== 0 && operation[operation.current][0] !== '-') {
+                operation[operation.current] += key.defaultValue;
+            }
+        } else if (key.defaultValue !== '.') {
+            operation[operation.current] += key.defaultValue;
+        }
+    } else if (key.className === 'operator' && (operation[operation.current].length >= 2 ||
+                operation[operation.current].length !== 0 && operation[operation.current][0] !== '-')) {
+        if (operation.result !== '') {
+            operation.first = operation.result;
+            operation.op = '';
+            operation.second = '';
+            operation.current = 'first';
+            operation.result = '';
+        }
 
-function mod(a, b) {
-    return a % b;
-}
-
-function power(a, b) {
-    return a ** b;
-}
-
-
-function operate(key) {
-    if (operation.current === null || operation.current === 1) {
-        operation.first = key;
-        operation.current = 2;
-    }else if (operation.current === 2) {
-        operation.op = key;
-        operation.current = 3;
-    } else {
-        operation.second = key;
-        operation.current = 3;
+        if (operation.current === 'first') {
+            operation.op = (key.defaultValue === 'xʸ') ? '^' : key.defaultValue;
+            operation.current = 'second';
+        } else {
+            operation.first = String(operation.calc());
+            operation.second = '';
+            operation.op = (key.defaultValue === 'xʸ') ? '^' : key.defaultValue;
+        }
+    } else if (key.className === 'equals' && operation.second.length !== 0) {
+        if (operation.second[0] !== '-' || operation.second.length >= 2) {
+            operation.result = String(operation.calc());
+            operation.current = 'first';
+        }
+    } else if (key.className === 'delete') {
+        if (key.id === 'clear-key') {
+            operation.first = '';
+            operation.op = '';
+            operation.second = '';
+            operation.current = 'first';
+            operation.result = '';
+        }else {
+            if (operation.result !== '') {
+                operation.result = operation.result.slice(0, operation.result.length - 1);
+            } else {
+                if (operation.second !== '') {
+                    operation.second = operation.second.slice(0, operation.second.length - 1);
+                }else if (operation.op !== '') {
+                    operation.op = '';
+                    operation.current = 'first';
+                } else {
+                    operation.first = operation.first.slice(0, operation.first.length - 1);
+                }
+            }
+        }
     }
+}
+
+
+function DisplayUpdate() {
+    let sup = document.querySelector('.sup');
+    let inf = document.querySelector('.inf');
+
+    let supText = operation.first;
+    supText += (operation.op !== '') ? ` ${operation.op}` : '';
+    supText += (operation.second !== '') ? ` ${operation.second}` : '';
+    sup.innerText = supText;
+
+    inf.innerText = operation.result;
 }
 
 
